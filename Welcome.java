6bd018b59c77a2b79140424dbd6a422443f173c3
lakshmi1908 <112061000+lakshmi1908@users.noreply.github.com>
2022-09-22 16:48:44 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,18 +1,24 @@
 public class Welcome {
 
 	public static void main(String[] args) {
+        int rows = 2, columns = 4;
+       
+        int[][] MatrixA = { {1, 1, 1, 1}, {2, 3, 5, 2} };
+        int[][] MatrixB = { {2, 3, 4, 5}, {2, 2, 4, -4} };
+    
+        int[][] sum = new int[rows][columns];
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sum[i][j] = MatrixA[i][j] + MatrixB[i][j];
+            }
+        }
 
-		        int num1, num2, sum;
-		        Scanner sc = new Scanner(System.in);
-		        System.out.println("Enter First Number: ");
-		        num1 = sc.nextInt();
-		        
-		        System.out.println("Enter Second Number: ");
-		        num2 = sc.nextInt();
-		        
-		        sc.close();
-		        sum = num1 + num2;
-		        System.out.println("Sum of these numbers: "+sum);
-		    }
-		}
-	
+        System.out.println("Sum of the given matrices is: ");
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(sum[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
+}
